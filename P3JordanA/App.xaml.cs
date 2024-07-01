5b37c36721d370ba1a59a532383b837b0569b56b
laboratorio <laboratorio@UPEAULA-31712>
2024-07-01 07:11:59 -0500

    Agregar archivos de proyecto.

---
+++ P3JordanA/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace P3JordanA
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ P3JordanA/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace P3JordanA
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
